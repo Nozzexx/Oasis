b68e244fbb129d7f2e5bf0333f73334b553fbf6a
Nozzexx <Nozzexx@gmail.com>
2024-12-01 02:43:52 -0600

    commit 12.01.24
    bug fixes

--- projectoasis/src/utils/db.ts
+++ projectoasis/src/utils/db.ts
@@ -5,25 +5,23 @@ if (!process.env.POSTGRES_HOST || !process.env.POSTGRES_USER || !process.env.POS
 }
 
 // Ensure the pool is a singleton in serverless environments
-let pool: Pool;
+const pool: Pool = global._pool || new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  ssl: {
+    rejectUnauthorized: false, // Required for some cloud databases like Heroku or AWS RDS
+  },
+  max: 10, // Limit the number of connections
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // Wait 2 seconds before timing out
+});
 
+// Test the connection if initializing
 if (!global._pool) {
-  global._pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: {
-      rejectUnauthorized: false, // Required for some cloud databases like Heroku or AWS RDS
-    },
-    max: 10, // Limit the number of connections
-    idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-    connectionTimeoutMillis: 2000, // Wait 2 seconds before timing out
-  });
-
-  // Test the connection
-  global._pool.connect((err, client, release) => {
+  pool.connect((err, client, release) => {
     if (err) {
       console.error('Error connecting to the database:', err.stack);
     } else {
@@ -31,8 +29,9 @@ if (!global._pool) {
       release();
     }
   });
-}
 
-pool = global._pool;
+  // Assign the pool to global._pool for reuse in serverless environments
+  global._pool = pool;
+}
 
 export default pool;
